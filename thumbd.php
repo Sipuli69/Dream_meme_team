a0e43b659d5fb7f9fa2a7065d4cae1d3818c7419
A-d-f <anna-maria.palm@student.hamk.fi>
2021-02-16 19:07:24 +0200

    siistiys

--- thumbd.php
+++ thumbd.php
@@ -2,7 +2,6 @@
 // session alku:
 session_start();
 
- 
 $yhteys = mysqli_connect("localhost", "trtkp20a3", "trtkp20a3passwd");
 if (!$yhteys) {
     die("Yhteyden muodostaminen epÃ¤onnistui: " . mysqli_connect_error());
@@ -12,16 +11,11 @@ if (!$tietokanta) {
     die("Tietokannan valinta epÃ¤onnistui: " . mysqli_connect_error());
 }
 //lasketaan down votesit ja tulostetaan downvotet $downv.
-
-
 $downv=mysqli_query($yhteys, "select sum(down) as countd from annamaria20101_li");
 while ($resultd=mysqli_fetch_array($downv))
 {
 echo $resultd['countd'];
 }
-
-
-
 
 // tallennetaan valuet (b-down painallukset) $value2:een.
 $value2=$_POST["value2"];
@@ -32,10 +26,7 @@ if($value2[b-down]) {
     $down=1;
 }
 
-
-
 //Seuraavaksi pitäisi tallentaa näitä tietoja tietokantaan..
-
 $yhteys = mysqli_connect("localhost", "trtkp20a3", "trtkp20a3passwd");
 if (!$yhteys) {
     die("Yhteyden muodostaminen epÃ¤onnistui: " . mysqli_connect_error());
@@ -52,8 +43,6 @@ mysqli_stmt_bind_param($stmt, 'ii', $up, $down);
 mysqli_stmt_execute($stmt);
 mysqli_stmt_close($stmt);
 mysqli_close($yhteys);
-
-
 
 exit;
 ?>
